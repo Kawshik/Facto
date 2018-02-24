8b93a25665714dbaacddd9667479f5cf54e5f10d
Kawshik <19342187+Kawshik@users.noreply.github.com>
2018-02-24 13:28:50 +0530

    Initial commit v1.0

---
+++ app/src/main/java/com/example/kawshik/facto/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.kawshik.facto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        final EditText txt = findViewById(R.id.txt);
+        final TextView txtView = findViewById(R.id.txtView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long num, fact = 1;
+                int i;
+
+                num = Long.parseLong(txt.getText().toString());
+                for(i=1;i<=num;i++) {
+                    fact = fact * i;
+                }
+
+                String res = String.valueOf(fact);
+                txtView.setText(res);
+            }
+        });
+    }
+}
